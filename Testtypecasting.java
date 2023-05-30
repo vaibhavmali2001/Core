eee22bce02bddc2eacbcd6247175df81f4cd9cbe
Vaibhav Mali <109029737+vaibhavmali2001@users.noreply.github.com>
2023-05-30 23:30:52 +0530

    Add files via upload

---
+++ Testtypecasting.java
@@ -0,0 +1,13 @@
+
+public class Testtypecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=34;
+		short s=(short)i;
+		long l=34;
+		float f=l;
+
+	}
+
+}
